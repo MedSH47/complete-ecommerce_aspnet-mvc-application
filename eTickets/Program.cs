ea2ddebcaad3e68c1410bd06f13338cfe78c3ac7
MedSH47 <harrabimohamedsaleh71@gmail.com>
2024-05-06 02:45:06 +0100

    save1

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,7 +1,16 @@
+using eTickets.Data;
+using eTickets.Data.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 var app = builder.Build();
 
@@ -24,4 +33,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+AppDbInitializer.Seed(app);
 app.Run();
+
